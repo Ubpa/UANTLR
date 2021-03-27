f86c65855a9f11ef1e73fa44fe22e10c545e9a29
ubpa <ustczt@mail.ustc.edu.cn>
2021-03-27 23:46:21 +0800

    update antlr version

---
+++ tools/test.h
@@ -0,0 +1,14 @@
+namespace Ubpa::Utopia
+{
+    class [[a("a")]] Compoennt {
+    public:
+        int b[3];
+        float d;
+        std::vector<int> d;
+    private:
+        [[ddd]]
+        int f;
+        void func(){}
+    };
+} // namespace Ubpa::Utopia
+
